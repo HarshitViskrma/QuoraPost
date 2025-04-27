d4e1fb7934cc0d10f82aa3ea4dae183c0ffc8afe
Harshit Vishwakarma <130080307+HarshitViskrma@users.noreply.github.com>
2025-04-27 23:10:59 +0530

    add new file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || quorapost.onrender.com;
 const path = require('path');
 const {v4: uuidv4} = require('uuid');
 
