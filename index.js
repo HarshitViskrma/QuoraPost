c0683d802c48c7d67cfc0f2d6f7b44cd4a6e72ea
Harshit Vishwakarma <130080307+HarshitViskrma@users.noreply.github.com>
2025-04-27 23:24:58 +0530

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || quorapost.onrender.com;
+const port = process.env.PORT || 8080;
 const path = require('path');
 const {v4: uuidv4} = require('uuid');
 
